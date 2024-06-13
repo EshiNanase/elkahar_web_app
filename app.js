2ca1db6e431c300752d108d1dfab3d97d72468dc
EshiNanase <piksasov.ad@ogpt.tech>
2024-06-14 00:30:08 +0300

    Добавил динамическое отображение уровня

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
+
+let params = new URLSearchParams(document.location.search);
+document.getElementById('level').innerHTML = `Уровень: ${params.get('level')}`
+document.getElementById('exp').innerHTML = `Опыт: ${params.get('exp')}`
